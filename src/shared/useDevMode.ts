a52ceaec5a8d68160b9559b909a0277794cbb6aa
Grigorii K. Shartsev <me@shgk.me>
2025-02-13 15:42:47 +0100

    chore(dev): allow devMode in the release
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/shared/useDevMode.ts
+++ src/shared/useDevMode.ts
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { computed } from 'vue'
 import { browserStorage } from './browserStorage.service.ts'
 import { useBrowserStorage } from './useBrowserStorage.ts'
 
@@ -11,9 +10,7 @@ import { useBrowserStorage } from './useBrowserStorage.ts'
  * Control dev mode for additional debug tools
  */
 export function useDevMode() {
-	const isDevMode = __CHANNEL__ === 'dev'
-		? useBrowserStorage(browserStorage, 'devMode', false)
-		: computed({ get: () => false, set: () => {} })
+	const isDevMode = useBrowserStorage(browserStorage, 'devMode', false)
 
 	return {
 		isDevMode,

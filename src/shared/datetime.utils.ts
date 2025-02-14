1046165c0fcea3bf88695365c434e47cc4631cfa
Grigorii K. Shartsev <me@shgk.me>
2025-02-14 23:35:49 +0100

    chore(deps): remove @nextcloud/moment
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/shared/datetime.utils.ts
+++ src/shared/datetime.utils.ts
@@ -54,10 +54,12 @@ export function formatRelativeTimeFromNow(dateOrMs: Date | number) {
 }
 
 /**
- * Convert milliseconds to the largest rounded unit
+ * Convert milliseconds to the largest unit rounded from 0.75 point.
  * @example 123 -> { value: 0, unit: 'second' }
  * @example 1000 -> { value: 1, unit: 'second' }
  * @example 25 * 60 * 60 * 1000 -> { value: 25, unit: 'minute' }
+ * @example 35 * 60 * 60 * 1000 -> { value: 35, unit: 'minute' }
+ * @example 45 * 60 * 60 * 1000 -> { value: 1, unit: 'hour' }
  * @example 3600000 -> { value: 1, unit: 'hour' }
  * @example 86400000 -> { value: 1, unit: 'day' }
  * @param ms - Duration in milliseconds
@@ -79,11 +81,14 @@ export function convertMsToLargestTimeUnit(ms: number): { value: number; unit: I
 	units.month = units.day / 30
 	units.year = units.day / 365
 
+	//
+	const round = (value: number) => Math.abs(value % 1) < 0.75 ? Math.trunc(value) : Math.round(value)
+
 	// Loop from the largest unit to the smallest
 	for (const key in units) {
 		const unit = key as keyof typeof units
 		// Round the value so 59 min 59 sec 999 ms is 1 hour and not 59 minutes
-		const rounded = Math.round(units[unit])
+		const rounded = round(units[unit])
 		// Return the first non-zero unit
 		if (rounded !== 0) {
 			return {

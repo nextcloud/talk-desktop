1bb63139b1b400230e2ddfa09a5e23a79044c406
Grigorii K. Shartsev <me@shgk.me>
2025-04-22 21:07:57 +0200

    refactor(user-status): use browser storage for status
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/talk/renderer/UserStatus/userStatus.store.ts
+++ src/talk/renderer/UserStatus/userStatus.store.ts
@@ -2,12 +2,9 @@
  * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-import type {
-	UserStatusPrivate,
-	UserStatusPublic,
-} from './userStatus.types.ts'
+import type { UserStatusPrivate, UserStatusPublic } from './userStatus.types.ts'
 import { defineStore } from 'pinia'
-import { computed, ref, watch } from 'vue'
+import { computed } from 'vue'
 import { emit, subscribe } from '@nextcloud/event-bus'
 import { getCurrentUser } from '@nextcloud/auth'
 import {
@@ -16,28 +13,13 @@ import {
 	heartbeatUserStatus,
 	updateUserStatus,
 } from './userStatus.service.ts'
+import { useBrowserStorage } from '../../../shared/useBrowserStorage.ts'
+import { browserStorage } from '../../../shared/browserStorage.service.ts'
 
 declare module '@nextcloud/event-bus' {
 	interface NextcloudEvents {
 		'user_status:status.updated': UserStatusPublic
 	}
-}
-
-/**
- * Cache the user status in local storage
- *
- * @param userStatus - User status
- */
-function cacheUserStatus(userStatus: UserStatusPrivate) {
-	localStorage.setItem('TalkDesktop:userStatus', JSON.stringify(userStatus))
-}
-
-/**
- * Restore the user status from local storage
- */
-function restoreUserStatus(): UserStatusPrivate | null {
-	// @ts-expect-error - JSON parse type is invalid in lib.ts, `null` is a valid value to parse
-	return JSON.parse(localStorage.getItem('TalkDesktop:userStatus'))
 }
 
 /**
@@ -56,7 +38,7 @@ function emitUserStatusUpdated(userStatus: UserStatusPublic) {
 }
 
 export const useUserStatusStore = defineStore('userStatus', () => {
-	const userStatus = ref<UserStatusPrivate | null>(null)
+	const userStatus = useBrowserStorage<UserStatusPrivate | null>(browserStorage, 'userStatus', null)
 
 	const isDnd = computed(() => userStatus.value?.status === 'dnd')
 
@@ -66,16 +48,8 @@ export const useUserStatusStore = defineStore('userStatus', () => {
 		}
 	})
 
-	// Restore the user status from cache
-	const cachedStatus = restoreUserStatus()
-	if (cachedStatus) {
-		setUserStatus(cachedStatus, true)
-	}
-
-	watch(userStatus, (newUserStatus) => cacheUserStatus(newUserStatus!), { deep: true })
-
 	const initPromise = (async () => {
-		await updateUserStatusWithHeartbeat(false, true)
+		setUserStatus(await fetchCurrentUserStatus())
 	})()
 
 	/**
@@ -98,7 +72,7 @@ export const useUserStatusStore = defineStore('userStatus', () => {
 	 * @param withEmit - Whether to emit the update event
 	 */
 	function patchUserStatus(newUserStatus: Partial<UserStatusPrivate>, withEmit: boolean = true) {
-		Object.assign(userStatus.value!, newUserStatus)
+		userStatus.value = { ...userStatus.value, ...newUserStatus } as UserStatusPrivate
 		if (withEmit) {
 			emitUserStatusUpdated(userStatus.value!)
 		}
@@ -132,17 +106,11 @@ export const useUserStatusStore = defineStore('userStatus', () => {
 	 * Update the user status with a heartbeat
 	 *
 	 * @param isAway - Whether the user is away
-	 * @param forceFetchStatus - Whether to force fetch the current status
 	 */
-	async function updateUserStatusWithHeartbeat(isAway: boolean, forceFetchStatus: boolean = false) {
+	async function updateUserStatusWithHeartbeat(isAway: boolean) {
 		const status = await heartbeatUserStatus(isAway)
-
 		if (status) {
 			setUserStatus(status)
-		} else if (forceFetchStatus) {
-			// heartbeat returns the status only if it has changed
-			// Request explicitly if forced to fetch status
-			setUserStatus(await fetchCurrentUserStatus())
 		}
 	}
 

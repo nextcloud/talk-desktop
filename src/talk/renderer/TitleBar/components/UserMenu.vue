a628eaa4a60b68bd55fefb05017b04677c2580f2
Grigorii K. Shartsev <me@shgk.me>
2025-07-03 15:35:51 +0200

    feat(title-bar): add Quit button to the user menu
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/talk/renderer/TitleBar/components/UserMenu.vue
+++ src/talk/renderer/TitleBar/components/UserMenu.vue
@@ -19,6 +19,7 @@ import IconChevronRight from 'vue-material-design-icons/ChevronRight.vue'
 import IconEmoticonOutline from 'vue-material-design-icons/EmoticonOutline.vue'
 import IconLogout from 'vue-material-design-icons/Logout.vue'
 import IconPencil from 'vue-material-design-icons/Pencil.vue'
+import IconPower from 'vue-material-design-icons/Power.vue'
 import UserStatusDialog from '../../UserStatus/UserStatusDialog.vue'
 import ThemeLogo from './ThemeLogo.vue'
 import UiMenu from './UiMenu.vue'
@@ -52,6 +53,7 @@ watch(isOpen, () => {
 const userProfileLink = generateUrl('/u/{userid}', { userid: user.id })
 
 const logout = window.TALK_DESKTOP.logout
+const quit = window.TALK_DESKTOP.quit
 
 /**
  * Handle user status type change
@@ -173,6 +175,15 @@ function handleUserStatusChange(status: UserStatusStatusType) {
 								<IconLogout :size="20" />
 							</template>
 							{{ t('talk_desktop', 'Log out') }}
+						</UiMenuItem>
+
+						<UiMenuSeparator />
+
+						<UiMenuItem tag="button" @click="quit">
+							<template #icon>
+								<IconPower :size="20" />
+							</template>
+							{{ t('talk_desktop', 'Quit') }}
 						</UiMenuItem>
 					</template>
 				</UiMenu>

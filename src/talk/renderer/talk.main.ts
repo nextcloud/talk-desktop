24e02f6528fb9a77d09dba901d640f78311870a3
Grigorii K. Shartsev <me@shgk.me>
2025-09-04 15:38:48 +0200

    fix(talk): hotfix for <a href=#> links breaking navigation
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/talk/renderer/talk.main.ts
+++ src/talk/renderer/talk.main.ts
@@ -16,3 +16,12 @@ import 'regenerator-runtime' // TODO: Why isn't it added on bundling
 await setupWebPage()
 
 await createTalkDesktopApp()
+
+// HOTFIX: prevent invalid links <a href="#"> used in NcListItem as a button from breaking routing in Vue Router 4 with Hash History
+// TODO: fix on NcListItem side and use real buttons instead of <a href="#">
+document.addEventListener('click', (event) => {
+	const emptyAnchorLink = (event.target as HTMLElement).closest('a[href="#"]')
+	if (emptyAnchorLink) {
+		event.preventDefault()
+	}
+})

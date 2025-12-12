447022c76f5fdaa624e8aee6263f904c3d4575d2
Grigorii K. Shartsev <me@shgk.me>
2025-12-12 09:17:43 +0100

    fix: add some missing initial state
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/shared/build.config.ts
+++ src/shared/build.config.ts
@@ -1,6 +1,8 @@
-/**
+/*!
  * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-export const BUILD_CONFIG = __BUILD_CONFIG__
+import type { BuildConfig } from 'build/BuildConfig.types.ts'
+
+export const BUILD_CONFIG: BuildConfig = __BUILD_CONFIG__

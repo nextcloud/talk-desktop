55fac01ef404ed47a3cf4a157fd6a128a434a56d
Grigorii K. Shartsev <me@shgk.me>
2026-05-15 14:34:53 +0200

    chore: update nextcloud styles
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

---
+++ resources/server-global-styles/34/meta.js
@@ -0,0 +1,20 @@
+/*!
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+/** Git ref the styles are extracted from: version tag (e.g. "v34.0.0rc1") or branch name (e.g. "master") */
+export const versionRef = 'v34.0.0rc1'
+/** Version git reference type used on styles extracting: "tag" or "branch" */
+export const versionRefType = 'tag'
+/** Version major number, e.g. 34 */
+export const versionMajor = 34
+/** Version from $OC_Version, e.g. "34.0.0.5" */
+export const version = '34.0.0.7'
+/** Version from $OC_VersionString, e.g. "34.0.0 RC 1" */
+export const versionString = '34.0.0 RC1'
+/** Commit hash, e.g. "a1b2c3d" */
+export const versionCommitHash = '5a02950'
+
+/** Available theming configurations */
+export const themingConfigs = [{ name: '', primaryColor: '', backgroundColor: '' }]

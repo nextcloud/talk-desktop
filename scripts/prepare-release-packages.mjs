a2a508fd0a22465c85133a287ecc38ce40859fa6
Grigorii K. Shartsev <me@shgk.me>
2026-09-20 22:21:16 +0200

    build(scripts): remove spreed clone in prepare-release-packages
    Covered by npm dependency and `.overrides/spreed/`
    
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- scripts/prepare-release-packages.mjs
+++ scripts/prepare-release-packages.mjs
@@ -7,9 +7,6 @@
 
 const packageJson = require('../package.json')
 
-const TALK_PATH = './out/.temp/spreed/'
-const talkDotGit = `${TALK_PATH}.git`
-
 $.quiet = true
 
 /**
@@ -27,15 +24,13 @@ function exit(message, code) {
  * Show help
  */
 function help() {
-	echo`Prepare release packages for Talk Desktop with Talk in ${TALK_PATH}
+	echo`Prepare release packages for Talk Desktop
 
-	Usage: npm run release:package -- --linux --mac --windows --version=v20.0.0
+	Usage: npm run release:package -- --linux --mac --windows
 	If no platform is specified, the current platform will be used.
-	If no version is specified, the stable version from package.json will be used.
 
 	Args:
 	--help - show help
-	--version - Optionally a specific Talk version/branch to build with, for example, v20.0.0-rc.1 or main. Default is package.json/talk.
 	--channel [CHANNEL] - Release channel: stable, beta, or dev. Default is stable.
 	--windows - build Windows package
 	--linux - build Linux package
@@ -49,13 +44,12 @@ function help() {
 }
 
 /**
- * Prepare release packages for Talk Desktop with Talk in TALK_PATH
+ * Prepare release packages for Talk Desktop
  *
  * @return {Promise<void>}
  */
 async function prepareRelease() {
 	const CHANNEL = process.env.CHANNEL || argv.channel || 'stable'
-	const TALK_VERSION = argv.version || packageJson.talk[CHANNEL]
 
 	// Default to the current platform
 	if (!argv.windows && !argv.linux && !argv.mac && !argv['mac-x64'] && !argv['mac-arm64']) {
@@ -63,69 +57,33 @@ async function prepareRelease() {
 		argv[platform] = true
 	}
 
-	echo`Packaging Nextcloud Talk v${packageJson.version} with Talk ${TALK_VERSION}...`
-
-	// Git wrapper for Talk repository
-	const gitSpreed = (command) => $`git --git-dir=${talkDotGit} --work-tree=${TALK_PATH} ${command}`
+	echo`Packaging Nextcloud Talk v${packageJson.version} ...`
 
 	// Check Talk Desktop repository
-	echo`[1/5] Checking for uncommitted changes in Talk Desktop${argv['skip-check'] ? ' (SKIPPED)' : '...'}`
+	echo`[1/4] Checking for uncommitted changes in Talk Desktop${argv['skip-check'] ? ' (SKIPPED)' : '...'}`
 	if (!argv['skip-check'] && (await $`git status -s`).stdout) {
 		exit('❌ You have uncommitted changes in the Talk Desktop repository', 1)
 	}
 
-	// Check and prepare Talk repository
-	echo`[2/5] Checking for Talk repository in ${TALK_PATH}`
-	if (fs.existsSync(TALK_PATH)) {
-		echo`- Talk has been found in ${TALK_PATH}`
-		echo`[3.1/5] Checking for uncommitted changes in Talk repository`
-		if ((await gitSpreed(['status', '-s'])).stdout) {
-			exit('❌ You have uncommitted changes in the Talk repository', 1)
-		}
-		echo`[3.2/5] Fetching Talk ${TALK_VERSION} from origin`
-		await spinner(
-			`Fetching Talk@${TALK_VERSION} from origin`,
-			() => gitSpreed(['fetch', '--no-tags', '--depth=1', 'origin', TALK_VERSION]),
-		)
-		echo`[3.3/5] Checkout Talk@${TALK_VERSION}`
-		await spinner(
-			`Checkout Talk@${TALK_VERSION}`,
-			() => gitSpreed(['checkout', 'FETCH_HEAD']),
-		)
-	} else {
-		echo`- No Talk has been found in ${TALK_PATH}`
-		echo`[3/5] Cloning Talk@${TALK_VERSION} to ${TALK_PATH}`
-		await spinner(
-			`Cloning Talk@${TALK_VERSION} to ${TALK_PATH}`,
-			() => $`git clone --branch=${TALK_VERSION} --depth=1 -- https://github.com/nextcloud/spreed ${TALK_PATH}`,
-		)
-	}
-
 	// (Re)-install Talk dependencies
-	echo`[4/5] Installing dependencies${argv['skip-install'] ? ' (SKIPPED)' : '...'}`
 	if (!argv['skip-install']) {
-		await spinner(
-			'[4.1/5] Installing dependencies in Talk Desktop',
-			() => $`npm ci`,
-		)
-
-		await spinner(
-			'[4.2/5] Installing dependencies in Talk',
-			() => $`npm ci --prefix ${TALK_PATH}`,
-		)
+		await spinner('[2/4] Installing dependencies ...', () => $`npm ci`)
+	} else {
+		echo`[2/4] Installing dependencies (SKIPPED)`
 	}
 
-	$.env.TALK_PATH = TALK_PATH
 	$.env.CHANNEL = CHANNEL
 
+	// TODO: add Talk version extraction and logging
+
 	// Styles override
-	await spinner('[4.3/5] Overriding Nextcloud styles', async () => {
+	await spinner('[3/4] Overriding Nextcloud styles', async () => {
 		const { overrideNextcloudStyles } = await import('./overrideNextcloudStyles.commands.mjs')
 		await overrideNextcloudStyles({ verbose: true })
 	})
 
 	// Build and package
-	echo`[5/5] Packaging...`
+	echo`[4/4] Packaging...`
 	argv.windows && await spinner('Package Windows', () => $`npm run build:windows && npm run package:windows`)
 	argv.linux && await spinner('Package Linux', () => $`npm run build:linux && npm run package:linux`)
 	argv.mac && await spinner('Package MacOS', () => $`npm run build:mac && npm run package:mac`)
